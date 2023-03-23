ad2a95dca15cf7d29dfb9f3bde332de8f479ac83
se-rondon <sergioe.rondon@gmail.com>
2023-03-23 15:00:05 +0100

    Implementacion de OAath2 y keycloack

---
+++ infraestructuradomain/keycloackAdapter/src/main/java/com/paymentchain/service/JwtService.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paymentchain.service;
+import java.net.URL;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwk.Jwk;
+import com.auth0.jwk.UrlJwkProvider;
+/**
+ *
+ * @author sotobotero
+ */
+@Service
+public class JwtService {
+
+    @Value("${keycloak.jwk-set-uri}")
+    private String jwksUrl;
+
+    @Value("${keycloak.certs-id}")
+    private String certsId;
+
+    @Cacheable(value = "jwkCache")
+    public Jwk getJwk() throws Exception {
+        URL url = new URL(jwksUrl);
+        UrlJwkProvider urlJwkProvider = new UrlJwkProvider(url);
+       Jwk get = urlJwkProvider.get(certsId.trim());  
+        return get;
+    }
+}
+
